d20a9c33363416d8f7a85992e0aec4157afbcaa1
CrespoRV <rodrigo-cs@live.com.ar>
2021-10-22 13:48:18 -0300

    Ultimo con form

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerText="HOLA";
+
+    // result.innerHTML="if you deposite " + principal + ",<br>" + "at an interest rate of " + rate + ",<br>" + "You will receive an amount of " +  interest + ",<br>" + "in the year " + year
     
 }
-        
+     
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
+
+
